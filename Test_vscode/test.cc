e2d56d5b4be6d02ef8ae32777ac87cf51e85eff7
gascsd <3311922913@qq.com>
2023-05-21 21:07:12 +0800

    测试set

--- Test_vscode/test.cc
+++ Test_vscode/test.cc
@@ -1,14 +1,31 @@
 #include <iostream>
-
+#include <set>
 
 using namespace std;
 
+void Test()
+{
+    int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+    set<int> s;
+    for(auto& e : a)
+    {
+        s.insert(e);
+    }
+    for(auto& e : s)
+    {
+        //e++;
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    cout << "hello vscode" << endl;
-    cout << "hello vscode" << endl;
-    cout << "hello vscode" << endl;
-    cout << "hello vscode" << endl;
-    cout << "hello vscode" << endl;
+    Test();
+    // cout << "hello vscode" << endl;
+    // cout << "hello vscode" << endl;
+    // cout << "hello vscode" << endl;
+    // cout << "hello vscode" << endl;
+    // cout << "hello vscode" << endl;
     return 0;
 }
